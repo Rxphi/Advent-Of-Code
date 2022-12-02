a737932d2005502d7a090dc50ec79354b1a57e0a
Rxphi <raphael.larisch@outlook.de>
2022-12-02 15:29:55 +0100

    Solved Day 1 2021

--- AoC2021/src/Day1.java
+++ AoC2021/src/Day1.java
@@ -1,9 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-
+		File input = new File("./src/input1.txt");
+		Scanner sc = new Scanner(input);
+		
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		
+		while (sc.hasNextInt()) {
+			nums.add(sc.nextInt());
+		}
+		
+		int partI = 0;
+		int partII = 0;
+		
+		for (int i = 0; i < nums.size() - 1; i++) {
+			if (nums.get(i) < nums.get(i+1)) {
+				partI++;
+			}
+		}
+		
+		for (int i = 1; i < nums.size() - 2; i++) {
+			if (nums.get(i-1) + nums.get(i) + nums.get(i+1) < nums.get(i) + nums.get(i+1) + nums.get(i+2)) {
+				partII++;
+			}
+		}
+		
+		System.out.println("Part I : " + partI);
+		System.out.println("Part II : " + partII);
 	}
 
 }
