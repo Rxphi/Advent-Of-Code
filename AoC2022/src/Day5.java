df7140eb0a005cd20cbedf4f3d8dc463acc81418
Rxphi <raphael.larisch@outlook.de>
2022-12-04 21:43:37 +0100

    Setup/Preparation of Day 5 2022

---
+++ AoC2022/src/Day5.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Day5 {
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		File input = new File("./src/input5.txt");
+		Scanner sc = new Scanner(input);
+		
+	}
+}
