4c78310ff6bbf8b7c956a5a305e43e0f454cf2f7
Rxphi <raphael.larisch@outlook.de>
2022-12-05 07:35:43 +0100

    Setup/Preparation for Day 6 of 2022

---
+++ AoC2022/src/Day6.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day6 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("./src/input6.txt");
+		Scanner sc = new Scanner(input);
+		
+		int partI = 0;
+		int partII = 0;
+		
+		System.out.println("Day VI");
+		System.out.println("Part I " + partI);
+		System.out.println("Part II " + partII);
+	}
+}
