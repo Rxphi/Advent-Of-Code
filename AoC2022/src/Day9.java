bce1e60f5ace89c9415cd08acb46ed9de11e57d1
Rxphi <raphael.larisch@outlook.de>
2022-12-08 06:37:27 +0100

    Setup/Preparation for Day 9

---
+++ AoC2022/src/Day9.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Day9 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("./inputFiles/test.txt");
+		Scanner sc = new Scanner(input);
+
+		int partI = 0; 
+		int partII = 0;
+		
+		System.out.println("Day IX");
+		System.out.println("Part I " + partI);
+		System.out.println("Part II " + partII);
+	}
+
+}
