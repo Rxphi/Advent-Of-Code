a8f83909839541aa635569aa1a07eae55d36d0a9
Rxphi <raphael.larisch@outlook.de>
2022-12-10 07:14:55 +0100

    Setup/Preparation Day 11

---
+++ AoC2022/src/Day11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class Day11 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("./inputFiles/test.txt");
+		Scanner sc = new Scanner(input);
+				
+		int partI = 0;
+		int partII = 0;
+		
+		System.out.println("Day XI");
+		System.out.println("Part I " + partI);
+		System.out.println("Part II " + partII);
+	}
+
+}
