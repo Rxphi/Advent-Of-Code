951ca22815eb495bc3c38981ee7b8f9888fcc890
Rxphi <raphael.larisch@outlook.de>
2022-12-14 08:01:12 +0100

    Solved Day 14 of 2022

--- AoC2022/src/Day14.java
+++ AoC2022/src/Day14.java
@@ -5,27 +5,37 @@ import java.io.FileNotFoundException;
 public class Day14 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File input = new File("./inputFiles/test.txt");
+		File input = new File("./inputFiles/input14.txt");
 		Scanner sc = new Scanner(input);
 		
-		int n = 0;
+		int maxWidth = 0;
+		int maxHeight = 0;
 		String line;
 		while(sc.hasNextLine() && !(line = sc.nextLine()).isEmpty()) {
 			String[] points = line.split(" -> ");		
 			for (String point : points) {
-				int pointi = Integer.parseInt(point.split(",")[0]);
-				int pointj = Integer.parseInt(point.split(",")[1]);
-				n = pointi > n ? pointi : n;
-				n = pointj > n ? pointj : n;
+				int pointj = Integer.parseInt(point.split(",")[0]);
+				int pointi = Integer.parseInt(point.split(",")[1]);
+				maxWidth = pointj > maxWidth ? pointj : maxWidth;
+				maxHeight = pointi > maxHeight ? pointi : maxHeight;
 			}
 		}
 		
-		Cave behindWaterfall = new Cave(new Scanner(input), n+1);
-		behindWaterfall.toString(0, 9, 494, 503);
+		Cave behindWaterfall = new Cave(new Scanner(input), maxWidth+1, maxHeight+1);
+		System.out.println("Input Cave");
+		behindWaterfall.printMe();	
+		int partI = behindWaterfall.simulateSandI();
+		System.out.println("After Part I Simulation");
+		behindWaterfall.printMe();
 		
-		int partI = 0;
-		int partII = 0;
+		behindWaterfall.resetForPartII();
+		System.out.println("After Reset for Part II");
+		behindWaterfall.printMe();
 		
+		int partII = behindWaterfall.simulateSandII();
+		System.out.println("After Part II Simulation");
+		behindWaterfall.printMe();
+		System.out.println("Scroll to the right --->\n");
 		System.out.println("Day XIV");
 		System.out.println("Part I : " + partI);
 		System.out.println("Part II : " + partII);
@@ -38,19 +48,20 @@ class Cave {
 	ArrayList<ArrayList<String>> map;
 	int width;
 	int height;
+	int shift;
 	
-	Cave (Scanner sc, int n) {
-		System.out.println("Map Size: " + n + " x " + n);
+	Cave(Scanner sc, int width, int height) {
+		System.out.println("Map Size: " + height + " x " + width);
 		map = new ArrayList<ArrayList<String>>();
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < height; i++) {
 			ArrayList<String> newRow = new ArrayList<String>();
-			for (int j = 0; j < n; j++) {
+			for (int j = 0; j < width; j++) {
 				newRow.add(".");
 			}
 			map.add(newRow);
 		}
-		width = n;
-		height = n;
+		this.width = width;
+		this.height = height;;
 		String line;
 		while(sc.hasNextLine() && !(line = sc.nextLine()).isEmpty()) {
 			addRockStructure(line);
@@ -64,35 +75,27 @@ class Cave {
 			int point1i = Integer.parseInt(points[i].split(",")[1]);
 			int point2j = Integer.parseInt(points[i+1].split(",")[0]);
 			int point2i = Integer.parseInt(points[i+1].split(",")[1]);
-			System.out.println("(" + point1j + "," + point1i + "), (" + point2j + "," + point2i + ")");
+			//System.out.println("(" + point1j + "," + point1i + "), (" + point2j + "," + point2i + ")");
 			int diffi = point2i - point1i;
 			int diffj = point2j - point1j;
 			
 			if (diffi == 0) {
 				if (diffj < 0) {
-					System.out.println("new line to the left");
 					for (int dj = 0; dj >= diffj; dj--) {
-						System.out.println("(" + (point1j+dj) + "," + point1i + ")");
 						map.get(point1i).set(point1j + dj, "#");
 					}
 				} else {
-					System.out.println("new line to the right");
 					for (int dj = 0; dj <= diffj; dj++) {
-						System.out.println("(" + (point1j+dj) + "," + point1i + ")");
 						map.get(point1i).set(point1j + dj, "#");
 					}
 				}
 			} else if (diffj == 0) {
 				if (diffi < 0) {
-					System.out.println("new line to the top");
 					for (int di = 0; di >= diffi; di--) {
-						System.out.println("(" + point1j + "," + (point1i+di) + ")");
 						map.get(point1i + di).set(point1j, "#");
 					}
 				} else {
-					System.out.println("new line to the bottom");
 					for (int di = 0; di <= diffi; di++) {
-						System.out.println("(" + point1j + "," + (point1i+di) + ")");
 						map.get(point1i + di).set(point1j, "#");
 					}
 				}
@@ -100,13 +103,118 @@ class Cave {
 		}
 	}
 	
-	public void toString(int starti, int endi, int startj, int endj) {
+	int simulateSandI() {
+		int out = 0;
+		boolean stable = true;
+		while (stable) {
+			int oldi = -1;
+			int oldj = 500;
+			int currenti = 0;
+			int currentj = 500;
+			while (oldi != currenti || oldj != currentj) {
+				if (currenti == height-1 || currentj == -1 || currentj == width) {
+					stable = false;
+					break;
+				}
+				if (map.get(currenti+1).get(currentj).equals(".")) { // check directly down
+					oldi = currenti;
+					currenti++;
+				} else if (map.get(currenti+1).get(currentj-1).equals(".")) { // check down left
+					oldi = currenti;
+					oldj = currentj;
+					currenti++;
+					currentj--;
+				} else if (map.get(currenti+1).get(currentj+1).equals(".")) { // check down right
+					oldi = currenti;
+					oldj = currentj;
+					currenti++;
+					currentj++;
+				} else { // place sand unit at its resting position
+					out++;
+					map.get(currenti).set(currentj, "o");
+					break;
+				}
+			}
+		}
+		return out;
+	}
+	
+	int simulateSandII() {
+		int out = 0;
+		boolean openSource = true;
+		while (openSource) {
+			int oldi = -1;
+			int oldj = 500 + shift;
+			int currenti = 0;
+			int currentj = 500 + shift;
+						
+			while (oldi != currenti || oldj != currentj) {
+				if (map.get(currenti+1).get(currentj).equals(".")) { // check directly down
+					oldi = currenti;
+					currenti++;
+				} else if (map.get(currenti+1).get(currentj-1).equals(".")) { // check down left
+					oldi = currenti;
+					oldj = currentj;
+					currenti++;
+					currentj--;
+				} else if (map.get(currenti+1).get(currentj+1).equals(".")) { // check down right
+					oldi = currenti;
+					oldj = currentj;
+					currenti++;
+					currentj++;
+				} else { // place sand unit at its resting position
+					out++;
+					map.get(currenti).set(currentj, "o");
+					break;
+				}
+			}
+			if (currenti == 0 && currentj == 500 + shift) { // source is blocked
+				map.get(currenti).set(currentj, "o");
+				break;
+			}
+		}
+		return out;
+	}
+	
+	void resetForPartII() {
+		shift = height;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (map.get(i).get(j).equals("o")) {
+					map.get(i).set(j, ".");
+				}
+			}
+			for (int x = 0; x < shift; x++) {
+				map.get(i).add(".");
+				map.get(i).add(0, ".");
+			}
+		}
+		
+		width += shift * 2;
+		height+=2;
+		System.out.println("New Map Size: " + height + " x " + width);
+		ArrayList<String> newRow1 = new ArrayList<String>();
+		ArrayList<String> newRow2 = new ArrayList<String>();
+		for (int j = 0; j < width; j++) {
+			newRow1.add(".");
+			newRow2.add("#");
+		}
+		map.add(newRow1);
+		map.add(newRow2);
+	}
+	
+	void printMe(int starti, int endi, int startj, int endj) {
 		for (int di = 0; di <= endi-starti; di++) {
 			for (int dj = 0; dj <= endj-startj; dj++) {
 				System.out.print(map.get(starti+di).get(startj+dj));
 			}
 			System.out.println();
 		}
+		System.out.println();
+	}
+	
+	void printMe() {
+		printMe(0, height-1, 0, width-1);
 	}
 }
 
