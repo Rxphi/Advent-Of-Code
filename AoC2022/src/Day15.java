89af98b58418181cfc4cbdc2fb71817f0addb29b
Rxphi <raphael.larisch@outlook.de>
2022-12-14 08:04:05 +0100

    Setup/Preparation for Day 15 of 2022

---
+++ AoC2022/src/Day15.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class Day15 {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		File input = new File("./inputFiles/test.txt");
+		Scanner sc = new Scanner(input);
+		
+		int partI = 0;
+		int partII = 0;
+		
+		System.out.println("Day XV"); 
+		System.out.println("Part I: " + partI);
+		System.out.println("Part II: " + partII);
+	}
+
+}
