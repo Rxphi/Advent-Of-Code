77eb60271eb6ac568340dae947fb2fd830b7232e
Rxphi <raphael.larisch@outlook.de>
2022-12-21 17:09:05 +0100

    2nd try of Part I of Day 20 of 2022

--- AoC2022/src/Day20.java
+++ AoC2022/src/Day20.java
@@ -5,158 +5,96 @@ import java.io.FileNotFoundException;
 public class Day20 {
 
 	public static void main(String[] args)throws FileNotFoundException {
-		File input = new File("./inputFiles/test.txt");
+		File input = new File("./inputFiles/input20.txt");
 		Scanner sc = new Scanner(input);
 		
-		LinkedList list = new LinkedList();
+		List<Integer> list = new LinkedList<Integer>();
+		while (sc.hasNextInt()) {
+			int x = sc.nextInt();
+			list.add(x);
+		}
+		int n = list.size();
+		// create arrays
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) arr[i] = list.get(i);
+		int[] swapped = new int[n]; // 0 = false and 1 = true (didnt want to write new swapleft and swapright)
 		
-		String line = "";
-		
-		while (sc.hasNextLine() && !(line = sc.nextLine()).isEmpty()) {
-			int v = Integer.parseInt(line);
-			list.add(new IntNode(v, list));
+		for(int current : list) {
 			
-		}
-		
-		ArrayList<Integer> order = list.toArrayList();
-		System.out.println("Iterator: " + order);
-		
-		Iterator<Integer> itr = order.iterator();
-		
-		while (itr.hasNext()) {
-			list.find(itr.next()).rotate();
-		}
-		
-		
-		
-		IntNode current = list.find(0);
-		
-		int partI = 0;
-		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 1000; j++) {
-				current = current.next;
+			int i = 0;
+			for(i = 0; i < n; i++) {
+				if (arr[i] == current && swapped[i] == 0) {
+					break;
+				}
 			}
-			partI += current.val;
+			int j = 0;
+			if (current < 0) {
+				for (j = 0; j > current; j--) {
+					swapleft(arr, i+j);
+					swapleft(swapped, i+j);
+				}
+			} else if (current > 0) {
+	
+				for (j = 0; j < current; j++) {
+					swapright(arr, i+j);
+					swapright(swapped, i+j);
+				}
+			}
+			swapped[Math.floorMod(i+j, n)] = 1;
+			/*for (int x : arr) {
+				System.out.print(x + " ");
+			}
+			System.out.println();*/
+		}
+				
+		int ind = 0;
+		for (ind = 0; ind < n; ind++) {
+			if (arr[ind] == 0) {
+				ind--;
+				break;
+			}
 		}
 		
+
+
+		System.out.println(arr[(ind + 1001) % n]);
+		System.out.println(arr[(ind + 2001) % n]);
+		System.out.println(arr[(ind + 3001) % n]);
+		int partI =  arr[(ind + 1001) % n] + arr[(ind + 2001) % n] + arr[(ind + 3001) % n];
 		
 		System.out.println("Day XX");
 		System.out.println("Part I : " + partI);
 		
 	}
-}
-
-class LinkedList {
-	IntNode head;
-	IntNode tail;
-	int size = 0;
 	
-	public IntNode get(int index) {
-		IntNode current = head;
-		for (int i = 0; i < index; i++) {
-			current = current.next;
+	public static void swapleft(int[] arr, int index) {
+		index = Math.floorMod(index, arr.length);
+		int temp = arr[index];
+		
+		if (index == 0) {
+			arr[0] = arr[arr.length-1];
+			arr[arr.length-1] = temp;
+		} else {
+			arr[index] = arr[index-1];
+			arr[index-1] = temp;
 		}
-		return current;
 	}
 	
-	public IntNode find(int value) {
-		IntNode current = head;
-		for(int i = 0; i < size; i++) {
-			if (current.val == value) {
-				return current;
-			}
-			current = current.next;
+	public static void swapright(int[] arr, int index) {
+		index = Math.floorMod(index, arr.length);
+		
+		int temp = arr[index];
+		
+		if (index == arr.length-1) {
+			arr[arr.length-1] = arr[0];
+			arr[0] = temp;
+		} else {
+			arr[index] = arr[index+1];
+			arr[index+1] = temp;
 		}
-		return null;
-	}
-	
-	public void add(IntNode node) {
-		if (head == null) {
-			head = node;
-			tail = node;
-		} else {	
-			tail.next = node;
-			node.prev = tail;
-			node.next = head;
-			head.prev = node;
-			tail = node;
-		}
-		size++;
-	}
-	
-	public void printMe() {
-		System.out.println(toArrayList());
-	}
-	
-	public ArrayList<Integer> toArrayList() {
-		ArrayList<Integer> out = new ArrayList<Integer>();
-		IntNode current = head;
-		for (int i = 0; i < size; i++) {
-			out.add(current.val);
-			current = current.next;
-		}
-		return out;
 	}
 }
 
-class IntNode {
-	IntNode prev;
-	IntNode next;
-	int val;
-	LinkedList parent;
-	
-	public IntNode(int val, LinkedList parent) {
-		this.val = val;
-		this.parent = parent;
-	}
-	
-	public void rotate() {
-		System.out.println("Rotating " + this.val);
-		if (val < 0) {
-			for (int i = val; i < 0; i++) {
-				this.prev.next = next;
-				this.next.prev = prev;
-				IntNode rem = prev;
-				this.prev = rem.prev;
-				this.next = rem;
-				rem.prev.next = this;
-				rem.prev = this;
-				
-				System.out.println("Head: " + parent.head.val + ", Tail: " + parent.tail.val);
-				if (parent.head.next == this) {
-					parent.tail = this;
-					parent.head = this.next;
-				} else if (parent.head == this) {
-					parent.head = this.next.next;
-				} else if (parent.tail == this) {
-					parent.tail = this.next;
-				}
-				parent.printMe();
-			}
-		} else if (val > 0) {
-			for (int i = 0; i < val; i++) {
-				this.prev.next = next;
-				this.next.prev = prev;
-				IntNode rem = next;
-				this.prev = rem;
-				this.next = rem.next;
-				rem.next.prev = this;
-				rem.next = this;
-				
-				System.out.println("Head: " + parent.head.val + ", Tail: " + parent.tail.val);
-				if (parent.head == this) {
-					parent.head = this.prev;
-				} else if (parent.tail == this) {
-					parent.tail = this.prev.prev;
-				} else if (parent.tail.prev == this) {
-					parent.tail = this;
-				}
-				parent.printMe();
-			}
-		}
-	}
-}
 
 
 
