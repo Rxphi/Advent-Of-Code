f11d79cdfaed09e72fc59717ef571bb673552391
Raphaël Larisch <raphael.larisch@outlook.de>
2023-12-14 07:04:15 +0100

    Started Day14 PartII of 2023, need more efficient solution...

--- AoC2023/Day14/main.c
+++ AoC2023/Day14/main.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+// #define N 10
+#define N 100
+
+char grid[N][N];
 static FILE *fp;
 
 void openFile();
 void readInput();
 void solvePartI();
 void solvePartII();
+void printGrid(char *, int, int);
 
-int main()
-{
+int main() {
     openFile();
 
     readInput();
@@ -22,34 +26,183 @@ int main()
     return 0;
 }
 
-void openFile()
-{
+void openFile() {
     fp = fopen("input.txt", "r");
 
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         perror("The file could not be opened!\n");
         exit(1);
     }
-    else
-    {
+    else {
         printf("Opened input file successfully!\n");
     }
 }
 
-void readInput()
-{
+void readInput() {
+    char row[N+1];
+    for (int i = 0; i < N; i++) {
+        fscanf(fp, "%s", row);
+        for (int j = 0; j < N; j++) {
+            grid[i][j] = row[j];
+        }
+    }
+    // printGrid(&grid[0][0], N, N);
     printf("Read input successfully!\n");
 }
 
-void solvePartI()
-{
+void tiltNorth() {
+    for (int j = 0; j < N; j++) {
+        int free = -1;
+        for (int i = 0; i < N; i++) {
+            if (grid[i][j] == '.' && free == -1) {
+                free = i;
+            } else if (grid[i][j] == '#') {
+                free = -1;
+            } else if (grid[i][j] == 'O' && free != -1 && free < i) {
+                grid[free][j] = 'O';
+                grid[i][j] = '.';
+                for (int k = free+1; k <= i; k++) {
+                    if (grid[k][j] == '.') {
+                        free = k;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // printf("===Tilted North===\n");
+    // printGrid(&grid[0][0], N, N);
+}
+
+void tiltWest() {
+    for (int i = 0; i < N; i++) {
+        int free = -1;
+        for (int j = 0; j < N; j++) {
+            if (grid[i][j] == '.' && free == -1) {
+                free = j;
+            } else if (grid[i][j] == '#') {
+                free = -1;
+            } else if (grid[i][j] == 'O' && free != -1 && free < j) {
+                grid[i][free] = 'O';
+                grid[i][j] = '.';
+                for (int k = free+1; k < N; k++) {
+                    if (grid[i][k] == '.') {
+                        free = k;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // printf("===Tilted West===\n");
+    // printGrid(&grid[0][0], N, N);
+}
+
+void tiltSouth() {
+    for (int j = 0; j < N; j++) {
+        int free = -1;
+        for (int i = N-1; i >= 0; i--) {
+            if (grid[i][j] == '.' && free == -1) {
+                free = i;
+            } else if (grid[i][j] == '#') {
+                free = -1;
+            } else if (grid[i][j] == 'O' && free != -1 && i < free) {
+                grid[free][j] = 'O';
+                grid[i][j] = '.';
+                for (int k = free-1; k >= 0; k--) {
+                    if (grid[k][j] == '.') {
+                        free = k;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // printf("===Tilted South===\n");
+    // printGrid(&grid[0][0], N, N);
+}
+
+void tiltEast() {
+    for (int i = 0; i < N; i++) {
+        int free = -1;
+        for (int j = N-1; j >= 0; j--) {
+            if (grid[i][j] == '.' && free == -1) {
+                free = j;
+            } else if (grid[i][j] == '#') {
+                free = -1;
+            } else if (grid[i][j] == 'O' && j < free) {
+                grid[i][free] = 'O';
+                grid[i][j] = '.';
+                for (int k = free-1; k >= 0; k--) {
+                    if (grid[i][k] == '.') {
+                        free = k;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // printf("===Tilted East===\n");
+    // printGrid(&grid[0][0], N, N);
+}
+
+void cycle() {
+    tiltNorth();
+    tiltWest();
+    tiltSouth();
+    tiltEast();
+}
+
+int northSupportBeamLoad() {
+    int load = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (grid[i][j] == 'O') {
+                load += N - i;
+            }
+        }
+    }
+    return load;
+}
+
+void solvePartI() {
     int solutionI = 0;
+
+    tiltNorth();
+    solutionI = northSupportBeamLoad();
+    
+    printGrid(&grid[0][0], N, N);
     printf("The solution to part I is: %d\n", solutionI);
 }
 
-void solvePartII()
-{
+void solvePartII() {
     int solutionII = 0;
+    tiltWest();
+    tiltSouth();
+    tiltEast();
+
+    // printf("After cycle 1:\n");
+    // printGrid(&grid[0][0], N, N);
+    for (int i = 0; i < 1000000000-1; i++) {
+    // for (int i = 0; i < 2; i++y) {
+        cycle();
+        // printf("After cycle %d:\n", i+2);
+        // printGrid(&grid[0][0], N, N);
+        if (i % 1000000 == 0) {
+            printf("%d\n", i);
+        }
+    }
+
+    solutionII = northSupportBeamLoad();
     printf("The solution to part II is: %d\n", solutionII);
+}
+
+void printGrid(char *arr, int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%1c", *(arr+j+width*i));
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
